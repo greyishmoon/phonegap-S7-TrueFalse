52753a2805efaba00f206cff8f651f633eeca152
greyishmoon <chris@blythe.plus.com>
2017-11-12 23:37:42 +0000

    random function

--- www/js/index.js
+++ www/js/index.js
@@ -20,30 +20,11 @@ function onDeviceReady() {
 
 function getAnswer() {
 	
-	$("#responseText").text("WORKS");
+	$("#responseText").text(random());
 	
 }
 
-//----------- CORDOVA ONLOAD ---------------- //
-
-//----------- HTML ONLOAD ---------------- //
-
-// JQuery ready event listener
-$( document ).ready( onReady );
-// or... $( window ).on( "load", readyFn );
-
-// JQuery DOM loaded event triggered function
-function onReady( jQuery  ) {
-    // MAIN CODE HERE
-    console.log("onReady");
-    
-    updateDisplay();
-}
-
-//----------- HTML ONLOAD ---------------- //
-
-
-// USE FOR ALL DISPLAY CODE
-function updateDisplay() {
-	$("#linked").text("Text replaced by updateDisplay");
+function random() {
+    return !Math.round(Math.random());
+    }
 }
