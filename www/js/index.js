18dffa52f5aca58e241455e5f2d0615549253255
greyishmoon <chris@blythe.plus.com>
2017-11-13 00:32:20 +0000

    Random button fixed

--- www/js/index.js
+++ www/js/index.js
@@ -2,40 +2,43 @@
 //----------- CORDOVA ONLOAD ---------------- //
 
 // Cordova device event listener - will not work in browser
-document.addEventListener("deviceready", onDeviceReady, false);
-		
+//document.addEventListener("deviceready", onDeviceReady, false);
+
+// TEMP HTML ONLOAD
+$( document ).ready( onDeviceReady );
+//		alert("JS");
 // Cordova device event triggered function
 function onDeviceReady() {
 	
-	// Add other event listeners here if needed (pause, resume, backbutton etc)
+    
+    // Add other event listeners here if needed (pause, resume, backbutton etc)
 	//set up click event handling for button
-  	$('#submitButton').on("click", getAnswer);
-	
+    $('#submitButton').on("click", getAnswer);
+    
     // updates display    
 	// updateDisplay();
 	    
 	console.log("device ready");
+    
+    alert("ONREADY");
 }
+
+
 
 
 function getAnswer() {
 	
-	$("#responseText").text("WORKS");
+    result = random();
+
+    if (result) {
+        $("#responseText").text("Result is TRUE");
+    } else {
+        $("#responseText").text("Result is FALSE");
+    }
+
 	
 }
 
 function random() {
     return !Math.round(Math.random());
-    }
 }
-
-
-//    result = random();
-//    $("#responseText").text("Result was: " + result);
-//    
-//    if (random()) {
-//        $("#responseText").text("TRUE");
-//    } else {
-//        $("#responseText").text("FALSE");
-//    }
-//	
